f2ddd29913d7adcd9cca5fe9c51ae12e7a95e173
Josh Chandler <j5d1c7@gmail.com>
2019-08-07 19:43:01 -0400

    added register/deregister capabilities to ?

--- chatbot/commandcenter/commands/office.py
+++ chatbot/commandcenter/commands/office.py
@@ -9,9 +9,34 @@ class OfficeCommand(Command):
     def __init__(self):
         self.name = "$office"
         self.help = "$office | Lists registered and unregistered devices on the CClub room network | Usage: $office"
-        self.author = "hellbacon"
-        self.last_updated = "April 20th 2019"
+        self.author = "hellbacon and spacedog"
+        self.last_updated = "August 7th 2019"
 
     def run(self, event_pack: EventPackage):
-        resp = requests.get("http://141.218.118.171:5001/json")
-        return resp.text
+        r = ""
+
+        if len(event_pack.body) == 3:
+            # command...
+            if event_pack.body[1] == "-r":
+                # register
+                nick = event_pack.sender.split(":")[0][1:] # gets username without :cclub.cs.wmich.edu
+                mac = event_pack.body[2]
+                text = requests.post("http://141.218.118.171:5001/reg", data={"nick": nick, "mac": mac}).text
+                if text == "success":
+                    r = "Successfully registered!"
+                else:
+                    r = "Something went wrong!"
+            elif event_pack.body[1] == "-d":
+                # deregister
+                nick = event_pack.sender.split(":")[0][1:] # gets username without :cclub.cs.wmich.edu
+                mac = event_pack.body[2]
+                text = requests.post("http://141.218.118.171:5001/dereg", data={"nick": nick, "mac": mac}).text
+                if text == "success":
+                    r = "Successfully registered!"
+                else:
+                    r = "Something went wrong!"
+        else:
+            # just a query
+            r = requests.get("http://141.218.118.171:5001/plain").text
+            
+        return r
