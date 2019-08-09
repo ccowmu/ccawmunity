e2804e6404d553c911b40dd93c55a7d8069a9444
Chase Myers <chasecool56@gmail.com>
2019-08-08 22:48:32 -0400

    Added ability to list your current registered mac addresses. (#52)
    * Added $office -l to list macs attatched to your nickname.
    * Whoops, readded spacedogs changes
    * Accidentally put deregsiter in wrong spot.

--- chatbot/commandcenter/commands/office.py
+++ chatbot/commandcenter/commands/office.py
@@ -8,7 +8,7 @@ import requests
 class OfficeCommand(Command):
     def __init__(self):
         self.name = "$office"
-        self.help = "$office | Lists people connected to CClub network | Usage: $office, register: $office -r <mac>, deregister: $office -d <mac>"
+        self.help = "$office | Lists people connected to CClub network | Usage: $office, register: $office -r <mac>, deregister: $office -d <mac>, list your macs: $office -l"
         self.author = "hellbacon and spacedog"
         self.last_updated = "August 7th 2019"
 
@@ -35,8 +35,17 @@ class OfficeCommand(Command):
                     r = "Successfully deregistered!"
                 else:
                     r = "Something went wrong!"
+        elif len(event_pack.body) == 2:
+            if event_pack.body[1] == "-l":
+                # list users mac
+                nick = event_pack.sender.split(":")[0][1:] # gets username without :cclub.cs.wmich.edu
+                text = requests.post("http://141.218.118.171:5001/list", data={"nick": nick}).text
+                if text == "failure":
+                    r = "There are no mac addresses registed for the username: " + nick
+                else:
+                    r = text
         else:
             # just a query
             r = requests.get("http://141.218.118.171:5001/plain").text
-            
+
         return r
