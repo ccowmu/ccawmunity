be7f860bb2c54f45bb61d18096c885d74e756f7a
Josh Chandler <j5d1c7@gmail.com>
2019-10-03 22:28:46 -0400

    door command

---
+++ chatbot/commandcenter/commands/door.py
@@ -0,0 +1,18 @@
+from ..command import Command
+from ..eventpackage import EventPackage
+import requests
+
+# note: this command only works when run on a machine in same subnet as dot.
+# (this command will work when run on dot.)
+
+class DoorCommand(Command):
+    def __init__(self):
+        self.name = "$door"
+        self.help = "$door | Tells the status of the cclub office door"
+        self.author = "sweeney and spacedog"
+        self.last_updated = "October 3rd 2019"
+
+    def run(self, event_pack: EventPackage):
+        r = requests.get("http://dot.cs.wmich.edu:8877").text
+
+        return r
