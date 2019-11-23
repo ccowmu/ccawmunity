634cce1402653475f5e26544888b38e475bce160
Xander Wardell <mascrinthus@gmail.com>
2019-11-22 23:10:24 -0500

    Just return text

--- chatbot/commandcenter/commands/inspire.py
+++ chatbot/commandcenter/commands/inspire.py
@@ -1,6 +1,7 @@
 from ..command import Command
 from ..eventpackage import EventPackage
 import requests
+import json
 
 # note: this command only works when run on a machine in same subnet as dot.
 # (this command will work when run on dot.)
@@ -8,10 +9,11 @@ import requests
 class MCOnlineCommand(Command):
     def __init__(self):
         self.name = "$inspire"
-        self.help = "$inspire | Posts an inspirational image"
+        self.help = "$inspire | Posts an inspirational quote"
         self.author = "presto"
         self.last_updated = "November 22nd 2019"
 
     def run(self, event_pack: EventPackage):
-        r = requests.get("https://inspirobot.me/api?generate=true")
-        return r.text
+        r = requests.get("https://inspirobot.me/api?generateFlow=1")
+        data = json.loads(r.content.decode("utf-8"))
+        return data["data"][1]["text"]
