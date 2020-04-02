46ba38ba780e6d450aa7fef5d4a7d34536cabf14
Josh Chandler <j5d1c7@gmail.com>
2020-04-02 13:37:25 -0400

    fixed crash on avatar change/name change/room join

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -33,6 +33,7 @@ from listenercenter.listenermanager import ListenerManager
 
 g_commander = Commander(botconfig.command_prefix, botconfig.command_timeout)
 
+
 def get_password():
     # try to find password in the BOT_PASSWORD environment variable
     if "BOT_PASSWORD" in environ and environ["BOT_PASSWORD"] is not "":
@@ -43,11 +44,13 @@ def get_password():
     return getpass.getpass(prompt='Password required for {}: '.format(botconfig.username))
 
 # verifies that a message is indeed invoking a command
+
+
 def is_command(command_string):
     if command_string[0:len(botconfig.command_prefix)] != botconfig.command_prefix:
         # doesn't start with command prefix
         return False
-    
+
     if re.match(r"\$\d+", command_string):
         # just a dollar amount, not a command
         return False
@@ -56,6 +59,8 @@ def is_command(command_string):
 
 # triggered when someone joins geeks.
 # member comes from event["sender"]
+
+
 def send_geeks_welcome_message(member):
     global g_client
 
@@ -64,21 +69,24 @@ def send_geeks_welcome_message(member):
     room.add_listener(on_message)
     print("Joined room: {}".format(room))
 
-    member_name = member.split(":")[0][1:] # gets username without :cclub.cs.wmich.edu
+    # gets username without :cclub.cs.wmich.edu
+    member_name = member.split(":")[0][1:]
 
     # send info
     with open("./static/welcome-message.txt", "r") as f:
         room.send_text(f.read().format(member_name))
 
 # called when a message is recieved.
+
+
 def on_message(room, event):
     global g_commander
-    
+
     if event['type'] == "m.room.member":
-        if event['membership'] == "join":
+        if event['content']['membership'] == "join":
             print("{0} joined".format(event['content']['displayname']))
 
-            # hack: fresh joins don't have event[unsigned][prev_data]
+            # hack: fresh joins don't have event[unsigned][prev_content]
             if 'unsigned' in event and 'prev_content' not in event['unsigned'] and event['room_id'] == botconfig.ROOM_ID_GEEKS:
                 send_geeks_welcome_message(event["sender"])
 
@@ -101,9 +109,11 @@ def on_message(room, event):
                 command_string = output[0]
 
                 print("Command detected: {0}".format(command_string))
-                event_package = EventPackage(body=output, room_id=event["room_id"], sender=event["sender"], event=event)
+                event_package = EventPackage(
+                    body=output, room_id=event["room_id"], sender=event["sender"], event=event)
 
-                response = g_commander.run_command(command_string, event_package)
+                response = g_commander.run_command(
+                    command_string, event_package)
 
                 # don't spam #geeks
                 if event['room_id'] == botconfig.ROOM_ID_GEEKS:
@@ -119,13 +129,17 @@ def on_message(room, event):
     try:
         # check if room is geeks and redact images/videos
         if event['room_id'] == botconfig.ROOM_ID_GEEKS and (event['content']['msgtype'] == "m.image" or event['content']['msgtype'] == "m.video"):
-            room.send_text(event['sender'] + " please post images in #img:cclub.cs.wmich.edu and link them here")
-            room.redact_message(event['event_id'], reason="Please post images in #img and link them here")
+            room.send_text(
+                event['sender'] + " please post images in #img:cclub.cs.wmich.edu and link them here")
+            room.redact_message(
+                event['event_id'], reason="Please post images in #img and link them here")
     except KeyError:
         # expecting matrix lib sync to be checking the key when it no longer exists
         pass
 
 # called when an invite is received.
+
+
 def on_invite(room_id, state):
     global g_client
 
@@ -140,7 +154,9 @@ def on_invite(room_id, state):
     print("Joined room: {}".format(room))
 
     # notice
-    room.send_text("(Note: after a bot restart, you will have to re-invite the bot to this room.)")
+    room.send_text(
+        "(Note: after a bot restart, you will have to re-invite the bot to this room.)")
+
 
 def main():
     global g_client
@@ -196,6 +212,7 @@ def main():
             pass
     except KeyboardInterrupt:
         print("Shutting down.")
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.WARNING)
