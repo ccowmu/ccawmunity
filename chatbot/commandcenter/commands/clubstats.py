2c52c17cd256bb894cd6bc1ff4ad7e82a13077f3
Adam Pohl <acpohl21@gmail.com>
2022-08-22 23:07:05 -0400

    Working on getting sniff working

--- chatbot/commandcenter/commands/clubstats.py
+++ chatbot/commandcenter/commands/clubstats.py
@@ -3,9 +3,11 @@
 # import the Command parent class, and the EventPackage class.
 from ..command import Command
 from ..eventpackage import EventPackage
+import requests
+from botconfig import clubstats_endpoint
 
 # create a class that inherits from Command
-class TemplateCommand(Command):
+class ClubstatsCommand(Command):
     # set the name and author of the command inside of its constructor.
     def __init__(self):
         self.name = "$clubstats" # this is required in order for the command to run!
@@ -17,12 +19,18 @@ class TemplateCommand(Command):
         self.last_updated = "August 22, 2022"
 
         self.is_nosy = True
+        self.last_sniffed = '...'
+        
 
 
     # define what the command does here.
     # this function must return a string. the string will be sent to the room.
     def run(self, event_pack: EventPackage):
-        return "Testing"
+        # print(event_pack)
+        return f"I smell... \"{self.last_sniffed}\"."
+
 
     def sniff_message(self, event_pack: EventPackage):
         self.last_sniffed = " ".join(event_pack.body)
+        r = requests.post(clubstats_endpoint + '/new', event_pack)
+        return r.status_code
