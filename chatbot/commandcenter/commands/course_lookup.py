c90bcbec3e3f1d16b3b555c17d98426340364462
Adam Pohl <acpohl21@gmail.com>
2023-03-26 16:37:20 -0400

    Arg parsing complete

--- chatbot/commandcenter/commands/course_lookup.py
+++ chatbot/commandcenter/commands/course_lookup.py
@@ -7,9 +7,10 @@ import requests
 from bs4 import BeautifulSoup
 
 BASE_URL = 'https://wmich.edu/classlookup/wskctlg.wskctlg_menu'
+ARG_LIST = ['-crn', '-crs']
 
 class ClassSection():
-    def __init__(self, crn: int, section, date, day, time, insm, loc, instructor, credit) -> None:
+    def __init__(self, crn: str, section: str, date: str, day: str, time: str, insm: str, loc: str, instructor: str, credit: str, is_lab: bool = False) -> None:
         self.crn = crn
         self.section = section
         self.date = date
@@ -19,6 +20,14 @@ class ClassSection():
         self.loc = loc
         self.instructor = instructor
         self.credit = credit
+        self.is_lab = is_lab
+    
+    def __str__(self):
+        return str(self.crn) + '' + str(self.section)
+    
+    def __repr__(self) -> str:
+        return str(self.crn) + '' + str(self.section)
+
 
 # create a class that inherits from Command
 class CourseLookupCommand(Command):
@@ -38,6 +47,7 @@ class CourseLookupCommand(Command):
     # define what the command does here.
     # this function must return a string. the string will be sent to the room.
     def run(self, event_pack: EventPackage):
+        args = self.parse_args(event_pack)
         form_data = {'ctrl':'catalog',
                      'lownum':'1',
                      'collapse': 'N',
@@ -49,8 +59,9 @@ class CourseLookupCommand(Command):
                      'insm': '',
                      'instr': '',
                      'attr': '',
-                     'subject': 'ADA'
+                     'subject': ''
                      }
+        form_data['crse'] = args['-crs']
         html = requests.post(BASE_URL, data=form_data).text
         soup = BeautifulSoup(html, 'html.parser')
         crns = soup.find_all('td', {'class': 'crn_td'})
@@ -64,16 +75,36 @@ class CourseLookupCommand(Command):
         credits = soup.find_all('td', {'class': 'crhr_td'})
         seats = soup.find_all('table', {'class': 'avail_seats'})
         courses = []
-        for i in range(len(crns)):
+        for i in range(len(crns) - 1):
             crn = crns[i].findChild('td').text
-            section = sections[i].findChild('td').text
-            date = dates[i].findChild('td').text
-            day = days[i].findChild('td').text
-            time = times[i].findChild('td').text
-            insm = insms[i].findChild('td').text
-            loc = locs[i].findChild('td').text
-            instructor = instructors[i].findChild('td').text
-            credit = credits[i].findChild('td').text
+            section = sections[i].text
+            date = dates[i].text
+            day = days[i].text
+            insm = insms[i].text
+            time = times[i].text
+            loc = locs[i].text
+            instructor = instructors[i].text
+            credit = credits[i].text
             # seat
-            courses.append(ClassSection(crn))
-        return courses[0].crn
+            courses.append(ClassSection(crn, section, date, day, insm, time, loc, instructor, credit))
+        return str(courses[0])
+
+
+    def parse_args(self, event_pack) -> dict:
+        arg_dict: dict = {}
+        # Default arguments -h and -v to false
+        arg_dict['-h'] = False
+        arg_dict['-v'] = False
+        
+
+        # Iterate through args and fill arg_dict
+        for i in range(len(event_pack.body)):
+            if(i == 0):
+                continue
+            elif(event_pack.body[i] == '-h'):
+                arg_dict['-h'] = True
+            elif(event_pack.body[i] == '-v'):
+                arg_dict['-v'] = True
+            elif(event_pack.body[i] in ARG_LIST and type(event_pack.body[i+1] == type(1))):
+                arg_dict[event_pack.body[i]] = event_pack.body[i + 1]
+        return arg_dict
