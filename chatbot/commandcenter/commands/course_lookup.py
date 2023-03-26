0102eb38e4bab20b60cfa4ce9f07791516c2e855
Adam Pohl <acpohl21@gmail.com>
2023-03-26 18:16:13 -0400

    Formats as code

--- chatbot/commandcenter/commands/course_lookup.py
+++ chatbot/commandcenter/commands/course_lookup.py
@@ -2,17 +2,28 @@
 
 # import the Command parent class, and the EventPackage class.
 from ..command import Command
+from ..command import CommandCodeResponse
 from ..eventpackage import EventPackage
 import requests
 from bs4 import BeautifulSoup
 
 BASE_URL = 'https://wmich.edu/classlookup/wskctlg.wskctlg_menu'
-ARG_LIST = ['-n', '-c', '-t', '-s']
+ARG_LIST = ['-n', '-c', '-t', '-s', '-h']
+HELP_TEXT = ''' $courselookup | Pull information from WMU Course Catalog
+Usage: $courselookup <args>
+Arguments:
+-c: CRN.
+-t: Semester, defaults to current semester.
+-s: Subject, defaults to CS.
+-h: Print help text.
+'''
 
 class ClassSection():
-    def __init__(self, crn: str, section: str, date: str, day: str, time: str, insm: str, loc: str, instructor: str, credit: str, is_lab: bool = False) -> None:
+    def __init__(self, crn: str, section: str, crse_num, crse_name, date: str, day: str, time: str, insm: str, loc: str, instructor: str, credit: str, is_lab: bool = False) -> None:
         self.crn = crn
         self.section = section
+        self.crse_num = crse_num
+        self.crse_name = crse_name
         self.date = date
         self.day = day
         self.time = time
@@ -23,7 +34,7 @@ class ClassSection():
         self.is_lab = is_lab
     
     def __str__(self):
-        return 'CRN: ' + str(self.crn) + 'SECTION: ' + str(self.section)
+        return self.crse_num + ' ' + self.crse_name + ' ' + 'CRN: ' + str(self.crn) + ' ' + 'SECTION: ' + str(self.section)
     
     def __repr__(self) -> str:
         return 'CRN: ' + str(self.crn) + 'SECTION: ' + str(self.section)
@@ -38,7 +49,7 @@ class CourseLookupCommand(Command):
         self.name = "$courselookup" # this is required in order for the command to run!
         
         # define what will be shown when someone calls "help $<your command>"
-        self.help = "$courselookup | Pull info on courses from WMU course catalog."
+        self.help = "$courselookup | Pull info on courses from WMU course catalog. Please use -h for more details"
         
         self.author = "acp"
         self.last_updated = "Mar 26, 2023"
@@ -48,10 +59,12 @@ class CourseLookupCommand(Command):
     # this function must return a string. the string will be sent to the room.
     def run(self, event_pack: EventPackage):
         args = self.parse_args(event_pack)
+        if(args['-h']):
+            return HELP_TEXT
         form_data = {'ctrl':'catalog',
                      'lownum':'1',
                      'collapse': 'N',
-                     'lines': '50',
+                     'lines': '20',
                      'term': '202340',
                      'campus': '',
                      'crse': '',
@@ -66,11 +79,11 @@ class CourseLookupCommand(Command):
         form_data['term'] = args['-t']
         form_data['subject'] = args['-s']
 
-
         html = requests.post(BASE_URL, data=form_data).text
         soup = BeautifulSoup(html, 'html.parser')
         crns = soup.find_all('td', {'class': 'crn_td'})
         sections = soup.find_all('td', {'class': 'section_td'})
+        crses = soup.find_all('td', {'class': 'crselink'})
         dates = soup.find_all('td', {'class': 'date'})
         days = soup.find_all('td', {'class': 'days'})
         times = soup.find_all('td', {'class': 'time'})
@@ -80,9 +93,12 @@ class CourseLookupCommand(Command):
         credits = soup.find_all('td', {'class': 'crhr_td'})
         seats = soup.find_all('table', {'class': 'avail_seats'})
         courses = []
-        for i in range(len(crns) - 1):
+        for i in range(len(crns)):
             crn = crns[i].findChild('td').text
             section = sections[i].text
+            crse = crses[i].findChild('a').get_text(separator='</b>').split('</b>')
+            crse_num = crse[0]
+            crse_name = crse[1]
             date = dates[i].text
             day = days[i].text
             insm = insms[i].text
@@ -90,14 +106,13 @@ class CourseLookupCommand(Command):
             loc = locs[i].text
             instructor = instructors[i].text
             credit = credits[i].text
-            # seat
-            courses.append(ClassSection(crn, section, date, day, insm, time, loc, instructor, credit))
-        out = ''
+            courses.append(ClassSection(crn, section, crse_num, crse_name, date, day, insm, time, loc, instructor, credit))
+        out = 'The following are the first 50 courses that fit your query\n'
         if(len(courses) == 0):
             return "No Courses Found"
         for course in courses:
             out += str(course) + '\n'
-        return out
+        return CommandCodeResponse(out)
 
 
     def parse_args(self, event_pack) -> dict:
