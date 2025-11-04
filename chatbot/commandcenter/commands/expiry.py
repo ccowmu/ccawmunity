4725b04625646373d469fe1fa348797471daa74f
Lochlan McElroy <85311595+lochlanmcelroy@users.noreply.github.com>
2025-11-04 15:04:19 -0500

    Refactor and improve $expiry LDAP command logic
    Simplifies LDAP connection handling, improves error messages, and refactors logic to reduce code repetition. Updates LDAP URL, removes unused SSL/TLS code, and adds permission checks and clearer responses for user and all-member expiry queries.

--- chatbot/commandcenter/commands/expiry.py
+++ chatbot/commandcenter/commands/expiry.py
@@ -3,7 +3,6 @@ from ..command import CommandCodeResponse
 from ..eventpackage import EventPackage
 
 import ldap3
-import ssl
 import string
 import time
 from datetime import date
@@ -15,95 +14,92 @@ class ExpiryCommand(Command):
         self.name = "$expiry"
         self.help = "$expiry | Gets a member's expiration date from their UID. | Usage: $expiry dolphin"
         self.author = "nothingbutflowers, sphinx"
-        self.last_updated = "Jan. 26, 2021"
+        self.last_updated = "Nov. 4, 2025"
         self.whitelist = ["crosstangent", "kahrl", "sphinx", "rezenee", "estlin"]
 
     def run(self, event_pack: EventPackage):
-        # TODO: redo the logic here so code is not repeated, I tried and failed
-        LDAP_URL = 'https://cclub.cs.wmich.edu/ldap/'
+        LDAP_URL = 'ldap://openldap:389'
         MEMBER_BASE = 'cn=members,dc=yakko,dc=cs,dc=wmich,dc=edu'
         POSIX_DAY = 86400
         DESIRED_FIELDS = ["shadowExpire"]
         
-        tls_config = ldap3.Tls(validate=ssl.CERT_REQUIRED)
         server = ldap3.Server(LDAP_URL)
-        conn = ldap3.Connection(server, user="cn=readonly,dc=yakko,dc=cs,dc=wmich,dc=edu", password=environ["LDAP_PASSWORD"], auto_bind="NONE")
-
-        conn.open()
-        conn.start_tls()
-        conn.bind()
+        conn = ldap3.Connection(
+            server, 
+            user="cn=readonly,dc=yakko,dc=cs,dc=wmich,dc=edu", 
+            password=environ.get("LDAP_PASSWORD", ""), 
+            auto_bind=True
+        )
 
         args = event_pack.body[1:]
         nick = event_pack.sender.split(":")[0][1:]
 
-        if len(args) == 0:
-            # no args given, assume sender wants own expiry
-            try:
+        try:
+            if len(args) == 0:
+                # no args given, assume sender wants own expiry
                 preparedQuery = "(uid=" + nick + ")"
                 conn.search(search_base=MEMBER_BASE,
                             search_filter=preparedQuery, attributes=DESIRED_FIELDS)
+                
+                if not conn.entries:
+                    return "I don't know who {} is!".format(nick)
+                
                 result = conn.entries[0]
                 if result["shadowExpire"].value == None:
                     # user doesn't expire
                     return "{} doesn't expire!".format(nick)
+                
                 unix_timestamp = result["shadowExpire"].value * POSIX_DAY
                 return "{}: {}".format(nick, str(date.fromtimestamp(unix_timestamp).strftime('%Y-%m-%d')))
-            except Exception as e:
-                print("something went wrong:", e)
-                return "this error message is incredibly helpful!"
-            finally:
-                conn.unbind()
-        else:
-            # check if arg is "-a" for all
-            if args[0] == "-a":
-                if nick in self.whitelist:
-                    try:
-                        preparedQuery = "(shadowExpire<=" + str(int(time.time()) // 86400) + ")"
-                        print(preparedQuery)
-                        conn.search(search_base=MEMBER_BASE,
-                                    search_filter=preparedQuery, attributes=["uid", "shadowExpire"])
-                        expiries = []
-                        for e in conn.entries:
-                            unix_timestamp = e["shadowExpire"].value * POSIX_DAY
-                            str_timestamp = str(date.fromtimestamp(unix_timestamp).strftime('%Y-%m-%d'))
-                            expiries.append(tuple((e["uid"].value, str_timestamp)))
-                        expiries = sorted(expiries, key=lambda x: x[0])
-
-                        result = ""
-                        for e in expiries:
-                            result += "{}: {}\n".format(e[0], e[1])
-                        return CommandCodeResponse(result)
-                    except Exception as e:
-                        print("aww fuck, I can't believe you've done this:", e)
-                    finally:
-                        conn.unbind()
+                
+            elif args[0] == "-a":
+                # Check all expired members
+                if nick not in self.whitelist:
+                    return "You don't have permission to use this command."
+                
+                preparedQuery = "(shadowExpire<=" + str(int(time.time()) // 86400) + ")"
+                conn.search(search_base=MEMBER_BASE,
+                            search_filter=preparedQuery, attributes=["uid", "shadowExpire"])
+                
+                if not conn.entries:
+                    return "No expired members found."
+                
+                expiries = []
+                for e in conn.entries:
+                    unix_timestamp = e["shadowExpire"].value * POSIX_DAY
+                    str_timestamp = str(date.fromtimestamp(unix_timestamp).strftime('%Y-%m-%d'))
+                    expiries.append((e["uid"].value, str_timestamp))
+                
+                expiries = sorted(expiries, key=lambda x: x[0])
+                
+                result = ""
+                for e in expiries:
+                    result += "{}: {}\n".format(e[0], e[1])
+                return CommandCodeResponse(result)
+            
             else:
-            # assume argument is a nick
-                try:
-                    preparedQuery = "(uid=" + args[0] + ")"
-                    conn.search(search_base=MEMBER_BASE,
-                                search_filter=preparedQuery, attributes=DESIRED_FIELDS)
-                    result = conn.entries[0]
-                    if result["shadowExpire"].value == None:
-                        # user doesn't expire
-                        return "{} doesn't expire!".format(args[0])
-                    unix_timestamp = result["shadowExpire"].value * POSIX_DAY
-                    return "{}: {}".format(args[0], str(date.fromtimestamp(unix_timestamp).strftime('%Y-%m-%d')))
-                except IndexError as e:
+                # Lookup specific user
+                preparedQuery = "(uid=" + args[0] + ")"
+                conn.search(search_base=MEMBER_BASE,
+                            search_filter=preparedQuery, attributes=DESIRED_FIELDS)
+                
+                if not conn.entries:
                     return "I don't know who {} is!".format(args[0])
-                except Exception as e:
-                    print("something went wrong:", e)
-                    return "this error message is incredibly helpful!"
-                finally:
-                    conn.unbind()
-        
-        # TODO: where should these errors go now?
-        # if len(event_pack.body) < 2:
-        #     return "Invalid usage of $expiry: not enough arguments. usage: $expiry dolphin"
-        # if len(event_pack.body) > 2:
-        #     return "Invalid usage of $expiry: too many arguments. $expiry does not support multiple lookups yet."
-        # if len(event_pack.body[1]) > 20:
-        #     return "Invalid usage of $expiry: provided member UID is too long."
-        # member_UID = event_pack.body[1]
-        # if member_UID.isalnum() is not True:
-        #     return "Invalid usage of $expiry: arguments must not contain non-alphanumeric characters"
+                
+                result = conn.entries[0]
+                if result["shadowExpire"].value == None:
+                    # user doesn't expire
+                    return "{} doesn't expire!".format(args[0])
+                
+                unix_timestamp = result["shadowExpire"].value * POSIX_DAY
+                return "{}: {}".format(args[0], str(date.fromtimestamp(unix_timestamp).strftime('%Y-%m-%d')))
+                
+        except ldap3.core.exceptions.LDAPException as e:
+            print("LDAP error:", e)
+            return "Failed to query LDAP server."
+        except Exception as e:
+            print("Unexpected error:", e)
+            return "An error occurred while processing your request."
+        finally:
+            if conn.bound:
+                conn.unbind()
