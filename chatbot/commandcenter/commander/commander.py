6215839f4bbb0f7801bb3192b35d572b13e4073b
airbreak404 <mail@tylerhoward.org>
2026-02-22 18:59:03 -0500

    Revert: restore command_not_recognized behavior

--- chatbot/commandcenter/commander/commander.py
+++ chatbot/commandcenter/commander/commander.py
@@ -117,7 +117,7 @@ class Commander:
 
                 return recv_end.recv()
             else:
-                return None
+                return self.command_not_recognized()
         else:
             # not a command, pass on to nosy commands
             for name, command in self.commands.items():
